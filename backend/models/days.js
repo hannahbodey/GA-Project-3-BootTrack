5c2fbccb57e67971843192ba9cc7a5e8d148ac72
hannahbodey <hannah.bodey@live.co.uk>
2023-03-22 16:49:05 +0000

    models and schemas created

---
+++ backend/models/days.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+//? Homework Schema - embedded in daysSchema
+const homeworkSchema = new mongoose.Schema({
+  homeworkTitle: { type: String, required: true, maxlength: 30 },
+  homeworkLink: { type: String, required: true },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+},
+{timestamps: true}
+)
+
+//? Classwork Schema - embedded in daysSchema
+const notesSchema = new mongoose.Schema({
+  notesTitle: { type: String, required: true, maxlength: 30 },
+  notesDescription: { type: String, required: true },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+},
+{timestamps: true}
+)
+
+//? Progress Schema - embedded in daysSchema
+const progressSchema = new mongoose.Schema({
+  completed: { type: Boolean, required: true },
+  confidenceRating: { type: Number, required: true, min: 1, max: 5 },
+  bookmarked: { type: Boolean, required: true },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+},
+{timestamps: true}
+)
+
+//? Days Schema - for data for each day 
+const daysSchema = new mongoose.Schema({
+  courseName: { type: String, required: true },
+  day: { type: Number, required: true },
+  week: { type: Number, required: true },
+  topicTitle: { type: String, required: true, maxlength: 20 },
+  classworkDescription: [{ type: String, required: true, maxlength: 250 }],
+  homeworkDescription: [{ type: String, required: false, maxlength: 250 }],
+  classworkNotes: [notesSchema],
+  homeworkUploads: [homeworkSchema],
+  progress: [progressSchema]
+})
+
+export default mongoose.model('Day', songSchema)
---
+++ backend/models/users.js
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+
+//? User Schema for all user profiles
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, maxlength: 15 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  //! These may be added as an extra once MVP is completed.
+  // teacher: { type: Boolean, required: true },
+  // course: { type: String, required: true, maxlength: 3 }
+})
+
+//? Virtual fields for the user: homework, notes, progress + password confirmation
+//? homework, notes, progress all brought in on relationship from daysSchema
+
+userSchema.virtual('homework', {
+  ref: 'Day',
+  localField: '_id',
+  foreignField: 'owner'
+})
+
+userSchema.virtual('notes', {
+  ref: 'Day',
+  localField: '_id',
+  foreignField: 'owner'
+})
+
+userSchema.virtual('progress',{
+  ref: 'Day',
+  localField: '_id',
+  foreignField: 'owner'
+})
+
+userSchema
+.virtual('passwordConfirmation')
+.set(function(userPasswordConfirmation){
+  this._passwordConfirmation = userPasswordConfirmation
+})
+
+//? To JSON command so that it removes password and shows the virtual fields
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform(doc, ret){
+    delete ret.password
+  }
+})
+
+//? Validation and authentication
+//Checking if password and password confirmation match
+userSchema.pre('validate', function(next){
+  if (this.isModified('password') && this.password !== this._passwordConfirmation){
+    this.invalidate('passwordConfirmation', 'Passwords do not match')
+  }
+  next()
+})
+
+//Encrypting password using bcrypt
+userSchema.pre('save', function(next){
+  if (this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(12))
+  }
+  next()
+})
+
+//Checking bcrypt password and inputted password at login
+userSchema.methods.validatePassword = function(nonCryptPassword){
+  return bcrypt.compare(nonCryptPassword, this.password)
+}
+
+export default mongoose.model('User', userSchema)
