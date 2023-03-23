c387425ed061a492d709e33361e99b2ee9797336
Neya <omar_omneya@outlook.com>
2023-03-23 14:42:00 +0000

    all routes in

--- config/router.js
+++ config/router.js
@@ -1,29 +1,27 @@
 import express from 'express'
-import { loginUser, registerUser } from '../controllers/auth.js'
-import { addClassNotes, updateClassNotes, deleteClassNotes } from '../controllers/classworkNotes.js'
-import { createHomework, updateHomework, deleteHomework } from '../controllers/homeworkNotes.js'
-import { secureRoute } from './secureRoute.js'
-import { getAllDays, getSingleDay } from '../controllers/days.js'
-import { putProgress } from '../controllers/progress.js'
-const router = express.Router()
 
-router.route('/api/days/:dayId/notes')
-  .post(secureRoute, addClassNotes)
+import { getAllDays, getSingleDay, createProgress } from '../controllers/days.js'
+import { createHomework, updateHomework, deleteHomework } from '../controllers/homework.js'
+import { addClassNotes, deleteClassNotes } from '../controllers/classworkNotes.js'
+import { registerUser, loginUser  } from '../controllers/auth.js'
+
+
+const router = express.Router()
 
 router.route('/api/register')
   .post(registerUser)
 
-router.route('/api/days')
-  .get(getAllDays)
-
 router.route('/api/login')
   .post(loginUser)
+
+router.route('/api/days')
+  .get(getAllDays)
 
 router.route('/api/days/:dayId')
   .get(getSingleDay)
 
 router.route('/api/days/:dayId/progress')
-  .post(putProgress)
+  .post(createProgress)
 
 router.route('/api/days/:dayId/homework')
   .post(createHomework)
@@ -33,7 +31,10 @@ router.route('/api/days/:dayId/homework/:homework')
   .delete(deleteHomework)
 
 router.route('/api/days/:dayId/notes/:notesId')
-  .put(updateClassNotes)
+  .put(addClassNotes )
   .delete(deleteClassNotes)
+
+
+
 
 export default router 
