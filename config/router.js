dc97272b0b331d6d3ab1a1190fe5251e007546ae
abedalisei <a14-3@hotmail.co.uk>
2023-03-24 11:11:22 +0000

    Finalising controller functions

--- config/router.js
+++ config/router.js
@@ -10,30 +10,26 @@ import { secureRoute } from '../config/secureRoute.js'
 
 const router = express.Router()
 
-router.route('/api/register') // ✅
+router.route('/api/register')
   .post(registerUser)
 
-router.route('/api/login') // ✅
+router.route('/api/login')
   .post(loginUser)
 
-router.route('/api/days') // ✅
+router.route('/api/days')
   .get(secureRoute, getAllDays)
 
-router.route('/api/days/:dayId') // ✅
+router.route('/api/days/:dayId')
   .get(secureRoute, getSingleDay)
 
-router.route('/api/days/:dayId/progress') // ?
+router.route('/api/days/:dayId/progress')
   .put(secureRoute, modifyProgress)
 
 router.route('/api/days/:dayId/homework')
   .put(secureRoute, modifyHomework)
   .delete(secureRoute, deleteHomework)
 
-// router.route('/api/days/:dayId/homework/:homework')
-//   .put(secureRoute, updateHomework)
-//   .delete(secureRoute, deleteHomework)
-
-router.route('/api/days/:dayId/notes') // ✅
+router.route('/api/days/:dayId/notes')
   .put(secureRoute, modifyClassNotes)
   .delete(secureRoute, deleteClassNotes)
 
