c73051682a9419ef86100d57d59e5160e0b9291c
abedalisei <a14-3@hotmail.co.uk>
2023-03-27 02:21:00 +0100

    Removing comments, logs, and spacing

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,39 +5,28 @@ import { assessError, Unauthorized, NotFound } from '../config/errors.js'
 
 const secret = process.env.SECRET
 
-// REGISTER ROUTE
-//Endpoint: /register
 export const registerUser = async (req,res) =>{
   try {
     const newUser = await User.create(req.body)
-
     return res.status(201).json({ message: `Welcome ${newUser.username}` })
   } catch (err) {
     return assessError(err, res)
   }
 }
 
-//LOGIN ROUTE
-//Endpoint: /login
 export const loginUser = async (req, res) =>{
   try {
     const { email, password } = req.body
-    
     const userToLogin = await User.findOne({ email: email })
-    console.log(userToLogin)
     if (!userToLogin) {
       throw new Unauthorized('Invalid credentials')
     }
-
     const userIsValidated = await userToLogin.validatePassword(password)
     if (!userIsValidated) {
       throw new Unauthorized('Invalid credentials')
     }
-
-    //JWT 
     const token = jwt.sign({ sub: userToLogin._id }, secret, { expiresIn: '7d' })
     return res.json({ message: `Welcome, ${userToLogin.username}`, token: token })
-
   } catch (err) {
     return assessError(err, res)
   }
@@ -45,13 +34,13 @@ export const loginUser = async (req, res) =>{
 
 export const loginDemo = async (req, res) => {
   try {
-    const demoUser = await User.findOne({ email: 'demo@demo.com' });
+    const demoUser = await User.findOne({ email: 'demo@demo.com' })
     if (!demoUser) {
-      throw new NotFound('Demo user not found');
+      throw new NotFound('Demo user not found')
     }
-    const token = jwt.sign({ sub: demoUser._id }, secret, { expiresIn: '7d' });
-    return res.json({ message: `Welcome, ${demoUser.username}`, token: token });
+    const token = jwt.sign({ sub: demoUser._id }, secret, { expiresIn: '7d' })
+    return res.json({ message: `Welcome, ${demoUser.username}`, token: token })
   } catch (err) {
-    return assessError(err, res);
+    return assessError(err, res)
   }
 }
