871f175ca91fb05e742d23e96c51e77c9ede15c2
Neya <omar_omneya@outlook.com>
2023-03-27 23:54:23 +0100

    front end

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,30 @@
+import Container from 'react-bootstrap/Container'
+import { Stack } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+
+
+
+
+
 const Home = () => {
   return (
-    <h1>Welcome to Home</h1>
+
+    <main className="home">
+      <Container>
+        <div className="hero">
+          <h1 className='display-4'>🏠Home page</h1>
+        </div>
+        <Container className= "hero">
+          <div className= "vstack gap-2">
+            <div className="bg-light border">Call to action - Sign Up to Join</div>
+            <Button to="#" as={Link} className='btn-register'>Register</Button>
+          </div>
+        </Container>
+      </Container>
+    </main>
+
+ 
   )
 }
 
--- controllers/days.js
+++ controllers/days.js
@@ -1,41 +1,27 @@
-import { filterDayByUser } from '../helper/filterDays.js'
-import { assessError, NotFound } from '../config/errors.js'
 import Day from '../models/days.js'
 
 export const getAllDays = async (req, res) => {
   try {
-    const days = await Day.find().lean()
-
-    console.log(days)
-
-    const filteredDays = days.map(day => {
-      day.progress = day.progress.filter(progress => progress.owner?.toString() === req.loggedInUser._id.toString())
-      day.homeworkUploads = day.homeworkUploads.filter(homework => homework.owner?.toString() === req.loggedInUser._id.toString())
-      day.classworkNotes = day.classworkNotes.filter(notes => notes.owner?.toString() === req.loggedInUser._id.toString())
-      return day
-    })
-
-    return res.json(filteredDays)
+    const days = await Day.find()
+    //res.status(200).json({ message: 'Get all days' })
+    return res.json(days)
   } catch (err) {
-    return assessError(err, res)
+    console.log(err)
   }
 }
 
 export const getSingleDay = async (req, res) => {
   try {
     const { dayId } = req.params
-    const day = await Day.findById(dayId).lean()
-
+    const day = await Day.findById(dayId)
     console.log('getting single day')
 
     if (!day) {
-      throw new NotFound('Day not found')
+      throw new Error('Day not found')
     }
-
-    filterDayByUser(day, req.loggedInUser._id)
 
     return res.json(day)
   } catch (err) {
-    return assessError(err, res)
+    console.log(err)
   }
 }
