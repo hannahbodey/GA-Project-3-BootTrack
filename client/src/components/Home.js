4c2dfb7629f19c509e8f1cd98ed59feab77811c3
Neya <omar_omneya@outlook.com>
2023-03-28 16:35:38 +0100

    home page completed

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,10 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react'
+
 import Container from 'react-bootstrap/Container'
 import test from '../images/test.png'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
-  const navigate = useNavigate()
-
-  //const [error, setError] = useState('')
-
-  const onClickLogin = async (e) => {
-    try {
-      const { data } = await axios.post('/api/login')
-      localStorage.setItem('PROJECT-3-TOKEN', data.token)
-      navigate('/login')
-    } catch (error) {
-      console.log(error)
-      //setError(error.response.data.message)
-    }
-  }
-  const onClickRegister = async (e) => {
-    try {
-      const { data } = await axios.post('/api/register')
-      localStorage.setItem('PROJECT-3-TOKEN', data.token)
-      navigate('/register')
-    } catch (error) {
-      console.log(error)
-      //setError(error.response.data.message)
-    }
-  }
 
   return (
 
@@ -38,14 +14,18 @@ const Home = () => {
           <h4>Your Dashboard Overview</h4>
         </div>
         <div className="hero">Call to action! <span>Sign Up to Join</span>
-          <button onClick={onClickLogin} className='btn-register'>Register</button>
-          <h5>If you are already registered</h5>
-          <button onClick={onClickRegister} className='btn-login'>Login</button>
+          <>
+          
+            <Link to='/register' className={location.pathname === '/register' ? 'active btn-register' : 'btn-register'}>Register</Link>
+            <h5>If you are already registered</h5>
+            <Link to='/login' className={location.pathname === '/login' ? 'active btn-login' : 'btn-login'}>Login</Link>
+        
+          </>
         </div>
         <div className="overView">
           <img src={test} alt="All days overview" />
         </div>
-      </Container>
+      </Container >
 
     </main >
 
