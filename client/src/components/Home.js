e3b1edb2a4deba5f2179984a43b302e6ea07acc2
Neya <omar_omneya@outlook.com>
2023-03-28 13:29:54 +0100

    home page navigate

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,35 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
-import { Link } from 'react-router-dom'
-import Button from 'react-bootstrap/Button'
-
 import test from '../images/test.png'
-
-
-
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+
+  //const [error, setError] = useState('')
+
+  const onClickLogin = async (e) => {
+    try {
+      const { data } = await axios.post('/api/login')
+      localStorage.setItem('PROJECT-3-TOKEN', data.token)
+      navigate('/login')
+    } catch (error) {
+      console.log(error)
+      //setError(error.response.data.message)
+    }
+  }
+  const onClickRegister = async (e) => {
+    try {
+      const { data } = await axios.post('/api/register')
+      localStorage.setItem('PROJECT-3-TOKEN', data.token)
+      navigate('/register')
+    } catch (error) {
+      console.log(error)
+      //setError(error.response.data.message)
+    }
+  }
+
   return (
 
     <main className="home">
@@ -16,9 +38,9 @@ const Home = () => {
           <h4>Your Dashboard Overview</h4>
         </div>
         <div className="hero">Call to action! <span>Sign Up to Join</span>
-          <button onClick= '/register' className='btn-register'>Register</button>
+          <button onClick={onClickLogin} className='btn-register'>Register</button>
           <h5>If you are already registered</h5>
-          <button onClick='/login' className='btn-login'>Login</button>
+          <button onClick={onClickRegister} className='btn-login'>Login</button>
         </div>
         <div className="overView">
           <img src={test} alt="All days overview" />
@@ -27,7 +49,7 @@ const Home = () => {
 
     </main >
 
- 
+
   )
 }
 
