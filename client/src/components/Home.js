db4d8cb796cb54749f4821de965a79efeccd949e
Neya <omar_omneya@outlook.com>
2023-03-29 11:41:25 +0100

    home page complete

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,34 @@
+import Container from 'react-bootstrap/Container'
+import test from '../images/test.png'
+import { Link } from 'react-router-dom'
+
+
 const Home = () => {
+
   return (
-    <h1>Welcome to Home</h1>
+
+    <main className="home">
+      <Container>
+        <div className="title">
+          <h4>Your Dashboard Overview</h4>
+        </div>
+        <div className="hero">Call to action! <span>Sign Up to Join</span>
+          <>
+          
+            <Link to='/register' className={location.pathname === '/register' ? 'active btn-register' : 'btn-register'}>Register</Link>
+            <h5>If you are already registered</h5>
+            <Link to='/login' className={location.pathname === '/login' ? 'active btn-login' : 'btn-login'}>Login</Link>
+        
+          </>
+        </div>
+        <div className="overView">
+          <img src={test} alt="All days overview" />
+        </div>
+      </Container >
+
+    </main >
+
+
   )
 }
 
