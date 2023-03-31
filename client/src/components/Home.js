b1bfc605bc4f68b85693c68b42333c388c46064c
hannahbodey <hannah.bodey@live.co.uk>
2023-03-31 10:18:55 +0100

    homepage slideshow complete

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,10 +3,11 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import completedDay from '../pictures/completed-day.png'
-import dayOneOverview from '../pictures/day-1-overview.png'
+import dayOneOverview from '../pictures/day-3-overview.png'
 import dailyProgressViewOne from '../pictures/demo-daily-progress-teacher-view.png'
 import dailyProgressViewTwo from '../pictures/newuser-daily-progress-teacher-view.png'
 import studentInitialView from '../pictures/student-initial-view.png'
+import studentUploadsView from '../pictures/upload-overview.png'
 
 const Home = () => {
 
@@ -19,14 +20,14 @@ const Home = () => {
   useEffect(() => {
     console.log('timeout hit')
     const interval = setInterval(() => {
-      { i < 4 ? i++ : (i === 4 ? i = i - 4 : i) }
+      { i < 5 ? i++ : (i === 5 ? i = i - 5 : i) }
       setClass()
     }, delay)
     return () => clearInterval(interval)
   }, [])
 
   const setClass = () => {
-    const classArray = ['completed-view', 'upload-view', 'progress-one-view', 'progress-two-view', 'student-view']
+    const classArray = ['completed-view', 'upload-view', 'progress-one-view', 'progress-two-view', 'student-view', 'uploads-overview']
     const activeClass = classArray[i]
     setIsActive(activeClass)
   }
@@ -57,28 +58,44 @@ const Home = () => {
     <main className='home'>
       <div className='overall-container'>
         <div className='slideshow'>
-          <div className="home-title">
+          <div className='home-title'>
             <h4>Choose BootTrack for all your student support services:</h4>
           </div>
           <div className={isActive === 'completed-view' ? 'image-display fade completed-view' : 'no-display'}>
-            <img src={completedDay} className='home-image' />
-            <h6 className='banner'>Allow students to mark days as completed</h6>
+            <div className='pic-container'>
+              <img src={completedDay} className='home-image' />
+              <h6 className='banner'>Allow students to mark days as completed</h6>
+            </div>
           </div>
           <div className={isActive === 'upload-view' ? 'image-display fade upload-view' : 'no-display'}>
-            <img src={dayOneOverview} className='home-image' />
-            <h6 className='banner'>Students can upload their work and notes, and mark their progress on any day</h6>
+            <div className='pic-container'>
+              <img src={dayOneOverview} className='home-image' />
+              <h6 className='banner'>Students can upload their work and notes, and mark their progress on any day</h6>
+            </div>
           </div>
           <div className={isActive === 'progress-one-view' ? 'image-display fade progress-one-view' : 'no-display'}>
-            <img src={dailyProgressViewOne} className='home-image' />
-            <h6 className='banner'>Teachers can view student progress and weekly updates</h6>
+            <div className='pic-container'>
+              <img src={dailyProgressViewOne} className='home-image' />
+              <h6 className='banner'>Teachers can view student progress and weekly updates</h6>
+            </div>
           </div>
           <div className={isActive === 'progress-two-view' ? 'image-display fade progress-two-view' : 'no-display'}>
-            <img src={dailyProgressViewTwo} className='home-image' />
-            <h6 className='banner'>Teachers can view student progress and weekly updates</h6>
+            <div className='pic-container'>
+              <img src={dailyProgressViewTwo} className='home-image' />
+              <h6 className='banner'>Teachers can view student progress and weekly updates</h6>
+            </div>
           </div>
           <div className={isActive === 'student-view' ? 'image-display fade student-view' : 'no-display'}>
-            <img src={studentInitialView} className='home-image' />
-            <h6 className='banner'>Students can view and access all days on their course, ready for interaction</h6>
+            <div className='pic-container'>
+              <img src={studentInitialView} className='home-image' />
+              <h6 className='banner'>Students can view and access all days on their course, ready for interaction</h6>
+            </div>
+          </div>
+          <div className={isActive === 'uploads-overview' ? 'image-display fade uploads-overview' : 'no-display'}>
+            <div className='pic-container'>
+              <img src={studentUploadsView} className='home-image' />
+              <h6 className='banner'>Students can view all homework and classwork uploads</h6>
+            </div>
           </div>
         </div>
         <div className='hero home-form-container'>
