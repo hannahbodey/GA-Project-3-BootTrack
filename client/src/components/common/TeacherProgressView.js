27143da046d123a8fa93c5872c1863c6ee6955a0
hannahbodey <hannah.bodey@live.co.uk>
2023-03-31 14:44:52 +0100

    report cards with more styling

--- client/src/components/common/TeacherProgressView.js
+++ client/src/components/common/TeacherProgressView.js
@@ -10,7 +10,7 @@ const TeacherProgressView = ({ filteredWork }) => {
           <div key={day._id} className='day'>
             <Card>
               <Card.Body >
-                {day.progress && <Card.Text className='owner'>Week: {day.week} Day: {day.day}</Card.Text>}
+                {day.progress && <Card.Text className='owner'>Week {day.week} Day {day.day}</Card.Text>}
                 {day.progress && day.progress.length > 0 && day.progress[0].completed === false ? <Card.Text>Work completed?<FontAwesomeIcon icon={icon({ name: 'circle-xmark' })} className='red-circle' /></Card.Text> : <Card.Text>Work completed? <FontAwesomeIcon icon={icon({ name: 'circle-check' })} className='green-circle' /></Card.Text>}
                 {day.progress && <Card.Text className='progress-class'>Confidence rating: {day.progress[0].confidenceRating}</Card.Text>}
                 {day.progress && day.progress.length > 0 && day.progress[0].bookmarked === false ? <Card.Text>Needs help?<FontAwesomeIcon icon={icon({ name: 'circle-xmark' })} className='red-circle' /></Card.Text> : <Card.Text>Needs help? <FontAwesomeIcon icon={icon({ name: 'circle-check' })} className='green-circle' /></Card.Text>}
--- client/src/components/common/TeacherReportView.js
+++ client/src/components/common/TeacherReportView.js
@@ -1,5 +1,7 @@
 import Card from 'react-bootstrap/Card'
 import { v4 as uuid } from 'uuid'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const TeacherReportView = ({ weeklyData }) => {
   return (
@@ -14,9 +16,9 @@ const TeacherReportView = ({ weeklyData }) => {
                   {day.week && <Card.Text className='owner'>Week {day.week}</Card.Text>}
                   {day.responses.length > 0 &&
                     <>
-                      <Card.Text className='progress-class'>Student comfort rating: {day.responses[0].overallComfort}</Card.Text>
-                      <Card.Text className='progress-class'>Student support rating: {day.responses[0].supportRating}</Card.Text>
-                      <Card.Text className='progress-class'>Student meeting request: {day.responses[0].contactPerson.length > 0 ? 'Yes' : 'No'}</Card.Text>
+                      <Card.Text className='progress-class'>Feels comfortable? {day.responses[0].overallComfort}</Card.Text>
+                      <Card.Text className='progress-class'>Feels supported? {day.responses[0].supportRating}</Card.Text>
+                      <Card.Text className='progress-class'>Wants a meeting? {day.responses[0].contactPerson.length > 0 ? <FontAwesomeIcon icon={icon({ name: 'circle-check' })} className='green-circle' /> : <FontAwesomeIcon icon={icon({ name: 'circle-xmark' })} className='red-circle' />}</Card.Text>
                     </>
                   }
                   {!day.responses.length > 0  && <p>Student has not yet completed this report</p>}
